0581e706b8ba1eca96d727f76a9ce6255ba8d07c
Ivelisse Mandato <mandatoi@bc.edu>
2019-09-22 17:59:37 -0400

    5.5 completed - save button not working

--- To Do List/DetailViewController.swift
+++ To Do List/DetailViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet weak var toDoField: UITextField!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
     var toDoItem: String?
     
     override func viewDidLoad() {
@@ -18,12 +19,26 @@ class DetailViewController: UIViewController {
         if let toDoItem = toDoItem{
             toDoField.text = toDoItem
         }
+        enableDisableSaveButton()
+        toDoField.becomeFirstResponder()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "UnwindFromSave" {
             toDoItem = toDoField.text
         }
+    }
+    
+    func enableDisableSaveButton() {
+        if let toDoFieldCount = toDoField.text?.count, toDoFieldCount > 0 {
+            saveBarButton.isEnabled = true
+        } else {
+            saveBarButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func toDoFieldChanged(_ sender: UITextField) {
+        enableDisableSaveButton()
     }
     
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
